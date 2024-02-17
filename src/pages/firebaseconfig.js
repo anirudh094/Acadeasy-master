052df4639d409bc6b90a9b27177843f7367945d3
anirudh094 <anirudhgarg094@gmail.com>
2024-02-17 13:37:09 +0530

    Firebase updated

--- src/pages/firebaseconfig.js
+++ src/pages/firebaseconfig.js
@@ -1,18 +1,19 @@
 import { initializeApp } from "firebase/app";
-import {getAuth,GoogleAuthProvider} from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHZ1g-7jUOVzq4-_W9MFBZxyhYmYjCyjU",
-  authDomain: "react01-9666b.firebaseapp.com",
-  databaseURL: "https://react01-9666b-default-rtdb.firebaseio.com",
-  projectId: "react01-9666b",
-  storageBucket: "react01-9666b.appspot.com",
-  messagingSenderId: "594389098469",
-  appId: "1:594389098469:web:c19796184f235b4678590c"
+  apiKey: "AIzaSyDs5VTZw5wt8LSRbcKMVW3kEK7XV8pt3fQ",
+  authDomain: "acad-easy.firebaseapp.com",
+  databaseURL: "https://acad-easy-default-rtdb.firebaseio.com",
+  projectId: "acad-easy",
+  storageBucket: "acad-easy.appspot.com",
+  messagingSenderId: "195865177272",
+  appId: "1:195865177272:web:358cfa5366bc34f77dd84d",
+  measurementId: "G-VH2FZ4GZ8K",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-export {auth,provider};
+export { auth, provider };
