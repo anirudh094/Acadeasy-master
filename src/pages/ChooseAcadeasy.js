dbad0fc39eda763f124086d4d42d92a77dbf8d19
anirudh094 <anirudhgarg094@gmail.com>
2024-04-28 11:58:14 +0530

    images changed

--- src/pages/ChooseAcadeasy.js
+++ src/pages/ChooseAcadeasy.js
@@ -4,50 +4,52 @@ import "../css/ChooseAcadeasy.css";
 function ChooseAcadeasy() {
   return (
     <div className="working-cy">
-      <h3>Why Choose <span>ACADEASY</span> ?</h3>
+      <h3>
+        Why Choose <span>ACADEASY</span> ?
+      </h3>
       <div className="working-cy-components">
-          <div className="working-cy-components-div">
-            <img
-              src="https://www.codeyoung.com/_ipx/w_1280/online-meeting.svg"
-              alt="logo"
-            ></img>
-            <p>Expert-Led Training</p>
-          </div>
-          <div className="working-cy-components-div">
-            <img
-              src="https://www.codeyoung.com/_ipx/w_1280/teacher.svg"
-              alt="logo"
-            ></img>
-            <p>Comprehensive Curriculum</p>
-          </div>
-          <div className="working-cy-components-div">
-            <img
-              src="https://www.codeyoung.com/_ipx/w_1280/online-class.svg"
-              alt="logo"
-            ></img>
-            <p>Live Training Sessions</p>
-          </div>
-          <div className="working-cy-components-div">
-            <img
-              src="https://www.codeyoung.com/_ipx/w_1280/schedule.svg"
-              alt="logo"
-            ></img>
-            <p>Industry Relevant Projects</p>
-          </div>
-          <div className="working-cy-components-div">
-            <img
-              src="https://www.codeyoung.com/_ipx/w_1280/Sandbox.svg"
-              alt="logo"
-            ></img>
-            <p>One On One Doubt Sessions</p>
-          </div>
-          <div className="working-cy-components-div">
-            <img
-              src="https://www.codeyoung.com/_ipx/w_1280/competition.svg"
-              alt="logo"
-            ></img>
-            <p>Industry-Recognized Certification</p>
-          </div>
+        <div className="working-cy-components-div">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/acad-easy.appspot.com/o/Choose%20Acadeasy%2FExpert-Led%20Training.png?alt=media&token=1eaa91ec-f8c7-42d2-867e-d04c6dfbc51d"
+            alt="logo"
+          ></img>
+          <p>Expert-Led Training</p>
+        </div>
+        <div className="working-cy-components-div">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/acad-easy.appspot.com/o/Choose%20Acadeasy%2FComprehensive%20Curriculum.png?alt=media&token=80991f82-0be4-402d-bb03-6d194096787f"
+            alt="logo"
+          ></img>
+          <p>Comprehensive Curriculum</p>
+        </div>
+        <div className="working-cy-components-div">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/acad-easy.appspot.com/o/Choose%20Acadeasy%2FLive%20Training%20Sessions.png?alt=media&token=8f4855ce-fae6-4fa5-835c-b8914c3a1501"
+            alt="logo"
+          ></img>
+          <p>Live Training Sessions</p>
+        </div>
+        <div className="working-cy-components-div">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/acad-easy.appspot.com/o/Choose%20Acadeasy%2FIndustry%20Relevant%20Projects.png?alt=media&token=f05e9e48-4803-4040-bd7b-79a41cc45f14"
+            alt="logo"
+          ></img>
+          <p>Industry Relevant Projects</p>
+        </div>
+        <div className="working-cy-components-div">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/acad-easy.appspot.com/o/Choose%20Acadeasy%2FOne%20On%20One%20Doubt%20Sessions.png?alt=media&token=987b148f-351c-4c0d-ad9f-bea59da6a9a8"
+            alt="logo"
+          ></img>
+          <p>One On One Doubt Sessions</p>
+        </div>
+        <div className="working-cy-components-div">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/acad-easy.appspot.com/o/Choose%20Acadeasy%2FIndustry-Recognized%20Certification.png?alt=media&token=4e0d61e6-824d-47a7-939c-61f8442e4300"
+            alt="logo"
+          ></img>
+          <p>Industry-Recognized Certification</p>
+        </div>
       </div>
     </div>
   );
