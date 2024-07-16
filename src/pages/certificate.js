8c7acdd4fb846515514dcae0074f188bc6320753
anirudh094 <anirudhgarg094@gmail.com>
2024-07-16 17:28:35 +0530

    Certificates added

--- src/pages/certificate.js
+++ src/pages/certificate.js
@@ -1,72 +1,57 @@
 import React from "react";
 import "../css/certificate.css";
-//import { AnimatedOnScroll } from "react-animated-css-onscroll";
+import { AnimatedOnScroll } from "react-animated-css-onscroll";
 
 function Certificate() {
   return (
     <> 
-      {/* <div className="certificate-body">
+      <div className="certificate-body">
         <h1>Certifications</h1>
         <h3>
-          On completion of a program each participant gets a course completion,
-          internship and outstanding performance certificates.
+        Upon completing the program, each participant receives a training completion certificate, an internship certificate, and an outstanding performance certificate.
         </h3>
         <AnimatedOnScroll animationIn="fadeInDownBig">
         <div className="certificate-grid">
           <div className="certificate-container">
             <img
-              src="https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_768,h_543/https://pianalytix.com/wp-content/uploads/2021/06/Pianalytix-Course-Completion-certificate-1406211-768x543.jpg"
+              src="https://firebasestorage.googleapis.com/v0/b/acad-easy.appspot.com/o/Certificates%2F3.png?alt=media&token=87471519-9da6-4ef2-a788-2509077e9cfe"
               alt="Avatar"
               className="certificate-image"
             />
             <div className="certificate-overlay">
               <div className="certificate-text">
-                Course Completion Certificate is awarded to you for the
-                dedication and time you have provided to learn and enhance your
-                skills during your training.
+              Training Completion Certificate is presented to you in recognition of your dedication and the time you invested in learning and enhancing your skills during your training.
               </div>
             </div>
           </div>
           <div className="certificate-container">
             <img
-              src="https://marketplace.canva.com/EAFCP1Ox_-c/1/0/1600w/canva-blue-yellow-minimalist-internship-certificate-RR9ND_Uk1Bw.jpg"
+              src="https://firebasestorage.googleapis.com/v0/b/acad-easy.appspot.com/o/Certificates%2F1.png?alt=media&token=c7dc16b7-66b6-4dfa-a333-7fe941ebb4be"
               alt="Avatar"
               className="certificate-image"
             />
             <div className="certificate-overlay">
               <div className="certificate-text">
-                Internship Completion Certificate is awarded to you after
-                gaining practical exposure and work experience by successful
-                completion of the capstone projects.
+              Internship Completion Certificate is granted to you in recognition of the practical exposure and work experience acquired through the successful completion of projects.
               </div>
             </div>
           </div>
           <div className="certificate-container">
             <img
-              src="https://i.pinimg.com/736x/38/a7/20/38a720fd9518980bd3cef1d22ee30c21.jpg"
+              src="https://firebasestorage.googleapis.com/v0/b/acad-easy.appspot.com/o/Certificates%2F2.png?alt=media&token=ae7c915f-dd55-46a1-aaed-29d62fd00c32"
               alt="Avatar"
               className="certificate-image"
             />
             <div className="certificate-overlay">
               <div className="certificate-text">
-                Outstanding Performer Certification is awarded to the
-                extraordinary students who have performed exceptionally well
-                during the training and internship periods.
+              The Outstanding Performer Certification is granted to exceptional students who have demonstrated remarkable performance throughout their training and internship periods.
               </div>
             </div>
           </div>
         </div>
         </AnimatedOnScroll>
-        <AnimatedOnScroll animationIn="wobble">
-        <div className="certificate-learners">
-          <img src="https://www.acmegrade.com/images/project/real_tag.svg" alt="certificate"/>
-          <p>
-            9 / 10 of our learners achieve their learning objectives after
-            successful course completion.
-          </p>
-        </div>
-        </AnimatedOnScroll>
-      </div> */}
+        
+      </div>
       
 
     </>
